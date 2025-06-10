91a338c2f9b3e9e82271d7911aad8ca2523f3632
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-06-10 17:30:17 -0300

    refactor: schema de cadastro com validação de CPF e ajustes de mensagens

--- src/schema/signupSchema.ts
+++ src/schema/signupSchema.ts
@@ -1,36 +1,43 @@
 // loginSchema.ts
 import { z } from "zod";
+import { isValidCPF } from "@/utils/verifyCpf";
 
-export const signupSchema = z.object({
-  email: z.string().min(1, "Preecha o email").email("Email inválido"),
-  password: z.string().min(1, "Preecha a senha"),
-  confirmpassword: z.string().min(1, "Campo confirmar senha está vazio"),
-      cpf: z
-        .string()
-        .length(11, "CPF deve ter 11 dígitos")
-        .regex(/^\d+$/, "CPF deve conter apenas números"),
-      address: z
-        .string()
-        .min(1, "Preecha o endereço")
-        .max(100, "Endereço deve ter no máximo 100 caracteres"),
-      number: z
-        .string()
-        .min(1, "Preecha o número")
-        .max(10, "Número deve ter no máximo 10 caracteres"),
-      city: z
-        .string()
-        .min(1, "Preecha a cidade")
-        .max(50, "Cidade deve ter no máximo 100 caracteres"),
-      state: z.string().length(2, "Estado deve ter exatamente 2 caracteres"),
-      zipcode: z
-        .string()
-        .length(8, "CEP deve ter exatamente 8 dígitos")
-        .regex(/^\d+$/, "CEP deve conter apenas números"),
+export const signupSchema = z
+  .object({
+    email: z.string().min(1, "Preecha o email").email("Email inválido"),
+    password: z.string().min(1, "Preecha a senha"),
+    confirmpassword: z.string().min(1, "Campo confirmar senha está vazio"),  
+    cpf: z.string()
+    .min(11, "CPF deve ter 11 dígitos")
+    .max(14, "CPF muito longo")
+    .refine((val) => val.replace(/\D/g, '').length === 11, {
+      message: "CPF deve conter 11 dígitos numéricos"
     })
-    .refine((data) => data.password === data.confirmpassword, {
-      message: "A senha e a confirmação de senha são diferentes",
-      path: ["confirmpassword"],
-})
-
+    .refine(isValidCPF, {
+      message: "CPF inválido"
+    })
+    .transform((val) => val.replace(/\D/g, '')),
+    address: z
+      .string()
+      .min(1, "Preecha o endereço")
+      .max(100, "Endereço deve ter no máximo 100 caracteres"),
+    number: z
+      .string()
+      .min(1, "Preecha o número")
+      .max(10, "Número deve ter no máximo 10 caracteres"),
+    city: z
+      .string()
+      .min(1, "Preecha a cidade")
+      .max(50, "Cidade deve ter no máximo 100 caracteres"),
+    state: z.string().length(2, "Estado deve ter exatamente 2 caracteres"),
+    zipcode: z
+      .string()
+      .length(8, "CEP deve ter exatamente 8 dígitos")
+      .regex(/^\d+$/, "CEP deve conter apenas números"),
+  })
+  .refine((data) => data.password === data.confirmpassword, {
+    message: "A senha e a confirmação de senha são diferentes",
+    path: ["confirmpassword"],
+  });
 
 export type SignupFormData = z.infer<typeof signupSchema>;
