a9c5e94dc6d2e8923288125270133ed91a966327
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-06-10 14:11:05 -0300

    feat: atualiza estilização do button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -7,7 +7,7 @@ interface ButtonProps {
 export const Button = ({ text, onClick }: ButtonProps) => {
   return (
     <button
-      className="bg-blue-600 text-white hover:bg-blue-700 focus:ring-blue-500 shadow-lg px-4 py-1 rounded-md"
+      className="bg-blue-600 text-white drop-shadow-lg hover:bg-blue-700 focus:ring-blue-500 shadow-lg px-4 py-1 rounded-md"
       onClick={onClick}
     >
       {text}
