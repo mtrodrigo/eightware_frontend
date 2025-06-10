2297399e3099ae011cd1252d8511350aba100689
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-06-10 15:05:07 -0300

    feat: adiciona propriedades type e disabled ao componente Button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,12 +3,15 @@ import React from "react";
 interface ButtonProps {
   text: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  type?: "submit";
+  disabled?: boolean
 }
-export const Button = ({ text, onClick }: ButtonProps) => {
+export const Button = ({ text, onClick, type, disabled }: ButtonProps) => {
   return (
     <button
       className="bg-blue-600 text-white drop-shadow-lg hover:bg-blue-700 focus:ring-blue-500 shadow-lg px-4 py-1 rounded-md"
-      onClick={onClick}
+      disabled={disabled}
+      type={type}
     >
       {text}
     </button>
