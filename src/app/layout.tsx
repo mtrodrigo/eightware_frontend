b8b5a9b67cf62fcc77201760e3013ae7d962ad42
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-06-11 08:55:59 -0300

    feat: adiciona suporte a notificações com react-hot-toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import {Toaster} from "react-hot-toast"
 
 export const metadata: Metadata = {
   title: "Frontend - Teste Técnico",
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body className="bg-slate-200 flex flex-col items-center justify-between min-h-screen">
         <Header />
+        <Toaster position="top-center" reverseOrder={false} />
         {children}
         <Footer />
       </body>
