3a59e0cfb7aca6319fad82d84ea0eaf546b7a208
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-06-11 16:00:22 -0300

    feat: adiciona a propriedade onClick ao componente Button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,14 +3,16 @@ import React from "react";
 interface ButtonProps {
   text: string;
   type?: "submit";
-  disabled?: boolean
+  disabled?: boolean;
+  onClick: () => void
 }
-export const Button = ({ text, type, disabled }: ButtonProps) => {
+export const Button = ({ text, type, disabled, onClick }: ButtonProps) => {
   return (
     <button
       className="bg-blue-600 text-white drop-shadow-lg hover:bg-blue-700 focus:ring-blue-500 shadow-lg px-4 py-1 rounded-md"
       disabled={disabled}
       type={type}
+      onClick={onClick}
     >
       {text}
     </button>
