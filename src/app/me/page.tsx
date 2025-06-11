fcf9ae3b27bdcf4c09946c2242e4b88e34861ad1
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-06-11 10:47:26 -0300

    feat: cria componente Me com estrutura básica

---
+++ src/app/me/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function Me() {
+  
+  return (
+    <main className="w-full flex items-center justify-center">
+      <section className="bg-slate-300 max-w-xs w-full rounded-md drop-shadow-xl py-8">
+        <h1>ME</h1>
+      </section>
+    </main>
+  );
+}
