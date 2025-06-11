8dc648c09c8ab1ba92d27990d194cc53994d3eab
Rodrigo Marques Tavares <rodrigour@gmail.com>
2025-06-11 08:39:40 -0300

    chore: update e config jest com typescript

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,27 @@
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
+  preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'babel-jest'
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.jest.json',
+        babelConfig: false,
+        useESM: true
+      }
+    ]
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)$': '<rootDir>/__mocks__/fileMock.js'
   },
   testPathIgnorePatterns: [
     '/node_modules/',
-    '/.next/'
-  ]
-}
+    '/.next/',
+    '/out/'
+  ],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx']
+};
